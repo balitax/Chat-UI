e561335c37de6fcd2016d5b96f4fd96be1d7a9a8
Agus Cahyono <cahyo.mamen@gmail.com>
2019-04-10 17:39:31 +0700

    Initial Commit

---
+++ HiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiApp
+//
+//  Created by Agus Cahyono on 10/04/19.
+//  Copyright © 2019 Agus Cahyono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
